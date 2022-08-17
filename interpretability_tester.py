c3e21624747ee655ec51cea08c5ce8693b65fe23
Macecode <danielmacedo.dc@gmail.com>
2022-08-17 08:46:47 -0300

    notebooks e scripts utilizados

---
+++ interpretability_tester.py
@@ -0,0 +1,224 @@
+# the present script provides the functions needed to generate a dataset with informations of each model passed
+
+import os
+import json
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2 as cv
+
+
+def generating_dataset(model, test_images, groundtruth, class_names):
+    
+    tumor_cells, annot_points = loading_json(groundtruth)
+    lst_classes, lst_heatmaps, lst_probs = getting_probs(test_images, model, class_names)
+    lst_tumors = checking_points(annot_points, lst_heatmaps)
+    tumor_region = []
+    accuracy = []
+    
+    # tumors found
+    tumor_cells_values = list(tumor_cells.values())
+    
+    for i, qtd in enumerate(lst_tumors):
+        if lst_classes[i] == 'benign':
+            tumor_region.append(0)
+            accuracy.append(0)
+            
+        else:
+            tumor_region.append(round((qtd/tumor_cells_values[i])*100, 2))
+            
+    
+    dataframe = pd.DataFrame(data=[tumor_cells.keys(), tumor_cells.values(), lst_classes, lst_probs, lst_tumors, tumor_region], index=['Case', 'Total Tumor Cells', 'Pred. Type', 'Probability (%)', 'Tumors Found' ,'Interpret. Region Accuracy (%)']).transpose()
+    
+    return dataframe
+    
+def checking_points(annot_points, lst_heatmaps):
+    
+    # list to check the correct values
+    correct_values = []
+    i = 0
+    
+    for k, v in annot_points.items():
+        got_it_right = 0
+        mask_matrix = lst_heatmaps[i]
+        
+        for a, b in enumerate(v):
+            if mask_matrix[b[1]][b[0]] > 0:
+                got_it_right += 1
+        # adding the correct correspondences to the list      
+        correct_values.append(got_it_right)
+        i += 1
+        
+    return correct_values
+
+def getting_probs(filen, model, class_names):
+    
+    # loading test images
+    probs = []
+    classes = []
+    masks = []
+    for root, dirs, items in os.walk(filen):
+        for item in items:
+            heatmap, class_, prob = generate_heatmap(filen+item, model, class_names)
+            mask = save_and_display_gradcam(filen+item, heatmap, pred=class_)
+            probs.append(round(prob*100,2))
+            classes.append(class_)
+            masks.append(mask)
+            
+
+    return classes, masks, probs
+
+def loading_json(filen):
+    tumor_cells = {}
+    annot_points = {}
+    
+    for root, dirs, items in os.walk(filen):
+        for i, item in enumerate(items):
+            # Opening JSON file
+            f = open(filen+item)
+            # returns JSON object as 
+            # a dictionary
+            data = json.load(f)
+            
+            # adding values to dictionary
+            tumor_cells[item.split('.')[0]] = len(data['tumor'])
+            
+            # creating list to get the annotations coordinates
+            x_y = []
+            for j, points in enumerate(data['tumor']):
+                # list to get the coordinates of one annotation
+                values_i_j = []
+                
+                # the values are multiplied by the image resolution
+                values_i_j.append(int(points['x']*1360))
+                values_i_j.append(int(points['y']*1024))
+                
+                # adding the coordinates to the list of one case
+                x_y.append(values_i_j)
+            
+            # adding corresponding annotations to the respective case
+            annot_points[item.split('.')[0]] = x_y
+
+            # Closing file
+            f.close()
+            
+    return tumor_cells, annot_points
+
+def save_and_display_gradcam(img_path, heatmap, alpha=0.9, pred=None): 
+    
+    # Load the original image
+    img = tf.keras.utils.load_img(img_path)
+    img = tf.keras.utils.img_to_array(img)
+
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
+
+    # Use jet colormap to colorize heatmap
+    if pred == 'malignant':
+        jet = cm.get_cmap("jet")
+    else:
+        jet = cm.get_cmap("Greens")
+
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = tf.keras.utils.array_to_img(jet_heatmap) 
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = np.array(jet_heatmap) # keras.preprocessing.image.img_to_array(jet_heatmap)
+    
+    # only for generating images
+    # Superimpose the heatmap on original image
+    superimposed_img = (jet_heatmap * alpha) + img
+    superimposed_img = tf.keras.utils.array_to_img(superimposed_img)
+    
+    # generating mask
+    jet_heatmap = cv.cvtColor(jet_heatmap, cv.COLOR_BGR2GRAY)
+    
+    # Otsu's thresholding
+    _, thresh = cv.threshold(jet_heatmap,0,255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    
+    # Display Grad CAM
+    plt.imshow(superimposed_img) # comentar quando for gerar o dataframe
+    
+    return thresh
+
+# Prepare image
+def generate_heatmap(img_path, model, class_names):
+    img_array = get_img_array(img_path, size=(128,128))
+    # Print what the top predicted class is
+    preds = model.predict(img_array)
+    model_preds = tf.squeeze(tf.round(preds))
+    top_pred_index = tf.argmax(preds[0])
+    
+    # Last Conv Layer Name
+    last_conv_layer_name = list(filter(lambda x: isinstance(x, keras.layers.Conv2D), model.layers))[-1].name
+
+    # Generate class activation heatmap
+    heatmap = make_gradcam_heatmap(img_array, model)
+    heat_test = heatmap.copy()
+    
+    # Display heatmap
+    print(heatmap)
+    plt.matshow(heatmap)
+    plt.show()
+    
+    return heatmap, class_names[top_pred_index], preds[0][top_pred_index]
+
+def get_img_array(img_path, size):
+
+    # `img` is a PIL image of size 299x299
+    img = tf.keras.utils.load_img(img_path, target_size=size)
+    
+    # `array` is a float32 Numpy array of shape (299, 299, 3)
+    array = tf.keras.utils.img_to_array(img)
+    
+    # We add a dimension to transform our array into a "batch"
+    # of size (1, 299, 299, 3)
+    array = np.expand_dims(array, axis=0)
+    return array
+
+
+def make_gradcam_heatmap(img_array, model, pred_index=None):
+    
+    # Last Conv Layer Name
+    last_conv_layer_name = list(filter(lambda x: isinstance(x, keras.layers.Conv2D), model.layers))[-1].name
+    
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model([model.inputs], 
+                                       [model.get_layer(last_conv_layer_name).output, model.output]) #base_model
+           
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, predictions = grad_model(img_array)
+        predictions = tf.squeeze(predictions)
+        if pred_index is None:
+            pred_index = tf.argmax(predictions)
+        class_channel = predictions[pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    
+    return heatmap.numpy()
